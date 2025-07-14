6150ebfb3a343b0ebf6bb64a4063c4a56dc4319a
José Pereira <contato@jspereiramoura.dev.br>
2025-07-14 20:44:03 -0300

    feat: add routing with react-router and create Home and ProductDetail pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
+import Home from "./pages/Home";
+import ProductDetail from "./pages/ProductDetail";
 
 function App() {
-  return <>Hello</>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/product/:id" element={<ProductDetail />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/ProductDetail.tsx
@@ -0,0 +1,8 @@
+export default function ProductDetail() {
+  return (
+    <div>
+      <h1>Product Detail Page</h1>
+      <p>This page will show the details of a specific product.</p>
+    </div>
+  );
+}
