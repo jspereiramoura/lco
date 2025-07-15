112f91a7dc43f0ba0f8f15ec0303a7babc9cab42
José Pereira <contato@jspereiramoura.dev.br>
2025-07-15 13:04:38 -0300

    feat: add Category and Product type definitions

---
+++ src/types/Product.d.ts
@@ -0,0 +1,11 @@
+declare interface Product {
+  id: number;
+  title: string;
+  slug: string;
+  price: number;
+  description: string;
+  category: Category;
+  images: string[];
+  creationAt: string;
+  updatedAt: string;
+}
