c8f95dffffd6fce46594f5bdef2c073294f21ef8
José Pereira <contato@jspereiramoura.dev.br>
2025-07-15 17:36:04 -0300

    chore: update dependencies and add MSW for testing

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,7 @@
 import "@testing-library/jest-dom";
+import { afterAll, afterEach, beforeAll } from "vitest";
+import { testServer } from "./mocks/setupDevServer";
+
+beforeAll(() => testServer.listen());
+afterEach(() => testServer.resetHandlers());
+afterAll(() => testServer.close());
